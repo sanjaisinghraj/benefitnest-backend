a2603be260eef10ab93938ff401977ec416cde40
sanjaisinghraj <sanjairajsingh1@gmail.com>
2025-12-25 13:10:02 +0530

    force CORS headers and allow OPTIONS globally

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ const cors = require("cors");
    INIT APP
 ========================= */
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 /* =========================
    CORS (MUST BE FIRST)
